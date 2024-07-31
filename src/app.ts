1eea5f04931f0d2d4c4333294cb42f8d4ee2f33b
lauCodx <lauphix1@gmail.com>
2024-07-31 03:15:50 +0100

    Added Error Handler Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import DbConnect from "./config/db.config";
-require("dotenv").config()
+import errorHandler from "./middlewares/errorHandler";
+require("dotenv").config();
 
-const app = express()
-const port = Number (process.env.PORT  || 5000)
+const app = express();
+const port = Number(process.env.PORT || 5000);
 
-DbConnect()
+DbConnect();
 
-app.listen(port, ()=>{
-    console.log("App listening on Port:", port)
-})
+app.listen(port, () => {
+  console.log("App listening on Port:", port);
+});
+
+app.use(errorHandler);
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import constants from "../constants";
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+    
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({ title: "Validation error", message: err.message });
+      break;
+
+    case constants.UNAUTHORIZED:
+      res.json({ title: "Unauthorize", message: err.message });
+      break;
+
+    case constants.FORBIDDEN:
+      res.json({ title: "Forbidden", message: err.message });
+      break;
+
+    case constants.NOT_FOUND:
+      res.json({ title: "Not found", message: err.message });
+      break;
+
+    case constants.SERVER_ERROR:
+      res.json({ title: "Server error", message: err.message });
+      break;
+
+    default:
+        console.error(err.message);
+        res.json({ title: "An error occurred", message: err.message });
+        break;
+  }
+};
+
+export default errorHandler;
